589a7417154f8ff5cd69dd44f4f68a3dc62979a5
shift-Anthony <anthonymoisescondori@gmail.com>
2025-10-29 19:00:43 -0500

    ejercicio 10

---
+++ javascript/ej10.js
@@ -0,0 +1,6 @@
+//funcion flecha para saludo
+let saludo = (nombre) => `Hola,${nombre}!!! Bienvenido al laboratorio 12.`;
+
+//Ejecucion del codigo
+let nombreUsuario = prompt("Ingrese su nombre:");
+alert(saludo(nombreUsuario));
