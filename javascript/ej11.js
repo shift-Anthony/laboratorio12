1faf4b390e3e14e54d6daa2f5bd5400c5137771c
shift-Anthony <anthonymoisescondori@gmail.com>
2025-10-29 19:06:25 -0500

    ejercicio11

---
+++ javascript/ej11.js
@@ -0,0 +1,13 @@
+//funcion flecha para saber si espositivo
+let esPositivo = (num) => {
+    if (num > 0) {
+        return `${num} es un número positivo.`;
+    } else if (num === 0) {
+        return `El número es cero. ni positivo ni negativo.`;
+    }
+    return `${num} no es un número positivo.`;
+}
+
+//Ejecucion del codigo
+let numero = parseInt(prompt("Ingrese un número:"));
+alert(esPositivo(numero));
